5844e8dc6eec800d8682d8625281beb396daf1ec
Andre <141501312+gdxfz7@users.noreply.github.com>
2024-05-16 15:09:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-
+<script> var cor1 = document.getElementById("cor1"); ... var body = document.getElementById("fundo"); function setGradient() { body.style.background = "linear-gradient(to right, " + … } </script> 
